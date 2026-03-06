d96460c921182e5abba87a0d95f8806d4401935b
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-03-06 19:00:21 +0530

    Python basics

---
+++ main.py
@@ -0,0 +1,191 @@
+#print("Hello World!")
+
+#module
+#import pyjokes
+
+#joke=pyjokes.get_joke()
+#print(pyjokes.get_joke())
+"""
+multline
+Commetn
+"""
+
+'''
+hello
+'''
+# print("hi")
+
+
+# Practice set 1
+
+# print(
+# """Twinkle, Twinkle, Little Star
+# Twinkle, twinkle, little star,
+# How I wonder what you are!
+# Up above the world so high,
+# Like a diamond in the sky.
+
+# Twinkle, twinkle, little star,
+# How I wonder what you are! ⭐"""
+# )
+
+# import text_to_image
+
+# encoded_image_path = text_to_image.encode("Hello World!", "image.png")
+# encoded_image_path = text_to_image.encode_file("hello.txt","output_image.png")
+
+# decoded_text = text_to_image.decode("encoded_image.png")
+# decoded_file_path = text_to_image.decode_to_file("encoded_image.png","output_text_file.txt")
+
+# import pyttsx3
+# engine = pyttsx3.init()
+
+# engine.say("okkkkkkkkkkkkkkkkkkkkkkkk")
+# engine.runAndWait()
+
+# import os
+# directory_path='/'
+
+# content=os.listdir(directory_path)
+
+# for item in content:
+#     print(item)
+
+# Variables and datatypes
+
+# a=1
+# b=2
+# print(a+b)
+
+# a="Dhanush"
+# b="Nayak"
+# print(a+" "+b)
+
+# a=1
+# b="string"
+# c=2
+# b=c
+# print(b)
+
+# print(bool(""))
+
+# e=None
+# print(e)
+
+# Arithmatic operator
+
+# a=1
+# b=2
+# print(a+b)
+# print(a-b)
+
+
+# a=1
+# b=2
+# print(a==b)
+
+# print(a>b)
+
+# print(a<b)
+
+# print(a!=b)
+
+# logical operator 
+
+# e= True or False
+# e=True and False
+# print(e)
+
+# a=True
+# print(not a)
+
+# type casting
+
+# a=1
+# print(type(a))
+
+# b=str(a)
+# print(type(b))
+
+# a=10.0
+# b=int(a)
+# print(type(b))
+
+# a=int(input("Enter a: "))
+# b=int(input("Enter a: "))
+# sum=a+b
+# print("sum is ", sum)
+
+# Practice set 2
+# a=10
+# b=20
+# print(a+b)
+
+# a=4
+# b=2
+# print(a%b)
+
+# n=input("Enter")
+# print(type(n))
+
+# a=34
+# b=80
+# print(a>b)
+
+# a=int(input("Enter a: "))
+# b=int(input("Enter b: "))
+# print("Average: ",(a+b)/2)
+
+# a=int(input("Enter a: "))
+# print("Square of",a,"is",a*a)
+
+# a=int(input("Enter a: "))
+# print("Square of",a,"is",a**2)
+
+# chapter 3 ->Strings -> Strings are immutable
+
+# a="Dhanush"
+# print(type(a))
+
+# name="Dhanush"
+# print(len(name))
+# nameshort=name[0:4]
+# print(nameshort)
+
+# word="amazing"
+# print(word[1:6:2]) #skip
+
+# name="DHANUSH"
+# print(len(name))
+# print(name.endswith("H"))
+# print(name.startswith("dH"))
+# print(name.capitalize())
+
+# name=input("Enter name: ")
+# print(name,"Good afternoon")
+
+# name="Dhanush"
+# date="13-03-2026"
+# print(f"""
+# Dear  {name},
+# You are selected!
+# {date},
+# """)
+
+
+
+# str="Hello  world"
+
+# # print(str.find("  "))
+# print(str.replace("  "," "))
+
+friends=["Rohan","Akash","Dhanush"]
+print(friends[0])
+friends[0]="shailesh"
+print(friends)
+
+
+
+
+
+
