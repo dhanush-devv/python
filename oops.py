807b2f9cb8409837407c2f01f3845c51d4bfa659
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-03-10 06:52:06 +0530

    python basics for data analytics

--- oops.py
+++ oops.py
@@ -90,3 +90,79 @@
 
 # print(a.company,b.company)
 # print(b.showLanguage("Dhanush","MERN"))
+
+
+# class TwoDVector:
+#     def __init__(self,i,j):
+#         self.i=i
+#         self.j=j
+
+#     def show(self):
+#         print(f"The vector is {self.i}i  +{self.j}j")
+
+
+# class ThreeDVector(TwoDVector):
+#     def __init__(self,i,j,k):
+#         super().__init__(i,j)
+#         self.k=k
+#     def show(self):
+#         print(f"The vector is {self.i}i  +{self.j}j +{self.k}k")
+
+# a=TwoDVector(1,2)
+# a.show()
+# b=ThreeDVector(1,2,3)
+# b.show()
+
+# class Animals:
+#     pass
+
+# class Pets(Animals):
+#     pass
+
+# class Dog(Pets):
+#     @staticmethod
+#     def bark():
+#         print("Dog Barks")
+
+# d=Dog()
+# d.bark()
+
+# class Employee:
+#     salary = 1233333
+#     increment = 50
+
+#     @property
+#     def salaryaAfterIncrement(self):
+#         return self.salary + self.salary * self.increment / 100
+
+#     @salaryaAfterIncrement.setter
+#     def salaryaAfterIncrement(self, salary):
+#         self.increment = ((salary / self.salary) - 1) * 100
+
+
+# a = Employee()
+
+# a.salaryaAfterIncrement = 280
+# print(a.increment)
+
+
+# class Complex:
+#     def __init__(self,r,i):
+#         self.r=r
+#         self.i=i
+
+#     def __add__(self,c2):
+#         return Complex(self.r + c2.r, self.i+c2.i)
+    
+#     def __str__(self):
+#         return f"{self.r}+{self.i}i"
+    
+
+# c1=Complex(1,2)
+# c2=Complex(3,4)
+
+# print(c1 + c2)
+
+
+
+        
